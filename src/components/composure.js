2f298e4371f442bd618ac4dfc2e6be54b0787de2
shubham Rawat <rawatshubam269@gmail.com>
2024-05-22 13:53:57 +0530

    React router dom lib setup done

--- src/components/composure.js
+++ src/components/composure.js
@@ -1,5 +1,6 @@
 import Header from "./header/Header";
 import Home from "./home/Home";
 import Feeds from "./feeds/Feeds";
+import NoPage from "./noPage/NoPage";
 
-export { Header, Home, Feeds };
+export { Header, Home, Feeds, NoPage };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, NoPage } from "./components/composure";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NoPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
