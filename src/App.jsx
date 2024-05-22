ddfc08316fb6f85302c1aa2c5bbf60881803bb9e
shubham Rawat <rawatshubam269@gmail.com>
2024-05-22 11:57:11 +0530

    Only mobile not desktop feature added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import { Header } from "./components/composure";
 function App() {
   return (
     <>
-      <div className="bg-orange-500 w-screen h-screen grid place-content-center md:hidden">
-        <div className="bg-white w-[20rem] h-[35rem] rounded-xl">
-          <Header />
-        </div>
+      <div className="bg-orange-500 w-screen h-screen sm:hidden">
+        <Header />
       </div>
-      <div className="hidden md:block">
+      <div className="hidden sm:block">
         <p>kindly switch to mobile version</p>
       </div>
     </>
